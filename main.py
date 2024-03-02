39ac86d9acc3d42311cafcd32d41fef3fdc2e196
b-junyoung <157205701+b-junyoung@users.noreply.github.com>
2024-03-02 04:54:13 +0000

    ACTUAL Final

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 num = input('Enter a number (decimal or integer): ')
 # type your code here
 
-#Sanitise the input
-  #take away spaces before and after with the value tool: .strip
-  #remove the dot with tool: replace
-  #strip the 0s on the left with tool: .lstrip
 num_modified = num.strip().replace(".", "").lstrip("0")
-
-#Count length
-  #tool: len()
 sf = len(num_modified)
-
-
 
 # do not change any code beyond this point
 print('The number', num, 'has', sf, 'significant figures.')
